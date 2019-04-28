43835f0d4804a1207759e8d886ba98842fc68529
hifuclub <hifuclub@outlook.com>
2019-04-28 14:30:12 +0800

    添加项目文件。

---
+++ netTEST/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace netTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Socket serverSocket = new Socket(AddressFamily.);
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
